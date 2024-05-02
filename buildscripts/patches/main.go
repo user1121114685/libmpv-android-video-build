52ccb0f5978e45dbe4871065a6de7383388f89fb
联盟少侠 <admin@shaoxia.xyz>
2024-05-02 19:30:18 +0800

    Update main.go

--- buildscripts/patches/main.go
+++ buildscripts/patches/main.go
@@ -34,11 +34,11 @@ func main() {
 	//replaceFile(mpv_path+"/player/loadfile.c", "    assert(mpctx->stop_play);\n\n    process_hooks(mpctx, \"on_after_end_file\");", "    mp_abort_playback_async(mpctx);\n    assert(mpctx->stop_play);\n\n    process_hooks(mpctx, \"on_after_end_file\");")
 
 	// 修复 部分华为手机黑屏的问题 兼容华为https://juejin.cn/post/7200191765516501049 主要是安卓9和安卓10的华为手机
-	replaceFile(mpv_path+"/video/out/gpu/video.c", "    const char *auto_fbo_fmts[] = {\"rgba16f\", \"rgba16hf\", \"rgba16\",", "    const char *auto_fbo_fmts[] = {\"rgb16f\",\"rgba16f\", \"rgba16hf\", \"rgba16\",")
+	// replaceFile(mpv_path+"/video/out/gpu/video.c", "    const char *auto_fbo_fmts[] = {\"rgba16f\", \"rgba16hf\", \"rgba16\",", "    const char *auto_fbo_fmts[] = {\"rgb16f\",\"rgba16f\", \"rgba16hf\", \"rgba16\",")
 	// 因为华为手机是 openGLes 3.2+ 所以 修改/video/out/opengl/formats.c On ES3.2+, both 16 bit floats work fully (except 3-component formats). 这部分就好了
 	//insertFile(mpv_path+"/video/out/gpu/ra.c", "    {RA_CTYPE_FLOAT, 4, {16, 16, 16, 16}, \"rgba16f\"},", "    {RA_CTYPE_FLOAT, 3, {16, 16, 16}, \"rgb16f\"},")
-	replaceFile(mpv_path+"/video/out/opengl/formats.c", "    {\"rgb16f\",  GL_RGB16F,   GL_RGB,             T_FL,  F_F16 | F_TF | F_ES32 | F_EXTF16},", "    {\"rgb16f\",  GL_RGB16F,   GL_RGB,             T_FL,  F_F16 | F_CF | F_ES32 | F_EXTF16},")
-	replaceFile(mpv_path+"/video/out/opengl/formats.c", "    {\"rgb16f\",  GL_RGB16F,   GL_RGB,             T_FL,  F_F16 | F_TF | F_ES3},", "    {\"rgb16f\",  GL_RGB16F,   GL_RGB,             T_FL,  F_F16 | F_CF | F_ES3},")
+	// replaceFile(mpv_path+"/video/out/opengl/formats.c", "    {\"rgb16f\",  GL_RGB16F,   GL_RGB,             T_FL,  F_F16 | F_TF | F_ES32 | F_EXTF16},", "    {\"rgb16f\",  GL_RGB16F,   GL_RGB,             T_FL,  F_F16 | F_CF | F_ES32 | F_EXTF16},")
+	// replaceFile(mpv_path+"/video/out/opengl/formats.c", "    {\"rgb16f\",  GL_RGB16F,   GL_RGB,             T_FL,  F_F16 | F_TF | F_ES3},", "    {\"rgb16f\",  GL_RGB16F,   GL_RGB,             T_FL,  F_F16 | F_CF | F_ES3},")
 	replaceFile(mpv_path+"/VERSION", "-UNKNOWN", "-little_lucky2_"+time.Now().Format("20060102"))
 
 	// 	//下面是 ffmpeg 补丁
