8a09f8e424c4295326f630995cb7952db89c8909
联盟少侠 <admin@shaoxia.xyz>
2024-10-02 18:35:27 +0800

    Update bundle_default.sh

--- buildscripts/bundle_default.sh
+++ buildscripts/bundle_default.sh
@@ -38,35 +38,6 @@ for arch in arm64-v8a armeabi-v7a x86 x86_64; do
   ln -sf "$(pwd)/app/build/outputs/apk/release/lib/$arch/libmediakitandroidhelper.so" "$jniLibs_dir/$arch"
 done
 
-cd ../..
-
-# --------------------------------------------------
-#
-#cd deps/media_kit/media_kit_native_event_loop
-#
-#flutter create --org com.alexmercerind --template plugin_ffi --platforms=android .
-#
-#if ! grep -q android "pubspec.yaml"; then
-#  printf "      android:\n        ffiPlugin: true\n" >> pubspec.yaml
-#fi
-#
-#flutter pub get
-#
-#cp -a ../../mpv/libmpv/. src/include/
-#
-#cd example || exit
-#
-#flutter clean
-#flutter build apk --release
-#
-#unzip -q -o build/app/outputs/apk/release/app-release.apk -d build/app/outputs/apk/release
-#
-#cd build/app/outputs/apk/release/ || exit
-#
-## --------------------------------------------------
-#
-#rm -r lib/*/libapp.so
-#rm -r lib/*/libflutter.so
 
 # 链接 c++shared.so
 c_shere_so=$(echo "$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/"*"/sysroot/usr/lib")
@@ -81,6 +52,8 @@ zip -r "default-arm64-v8a.jar"                lib/arm64-v8a
 zip -r "default-armeabi-v7a.jar"              lib/armeabi-v7a
 zip -r "default-x86.jar"                      lib/x86
 zip -r "default-x86_64.jar"                   lib/x86_64
+
+cd ../..
 
 mkdir -p ../../output
 
