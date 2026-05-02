de0da8361255ea6a459926b8281ea64009438040
shaoxia <admin@shaoxia.xyz>
2026-05-03 00:14:14 +0800

    更新依赖到最新版本

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -6,36 +6,36 @@ v_sdk=9123335_latest
 v_ndk=27.2.12479018
 v_sdk_build_tools=34.0.0
 
-v_libass=0.17.1
-v_harfbuzz=7.2.0
-v_fribidi=1.0.12
-v_freetype=2-13-0
-v_mbedtls=3.4.0
-v_dav1d=1.2.0
-v_libxml2=2.10.3
-v_ffmpeg=7.1.3
-v_mpv=32a164cc017acab50389f2194f720ccfd0b01a28
-v_libplacebo=c93aa134ab62365ce1177efff99b8e1e66a818e7
-v_lcms2=lcms2.17
-v_libogg=1.3.5
+v_libass=0.17.4
+v_harfbuzz=14.2.0
+v_fribidi=1.0.16
+v_freetype=2-14-3
+v_mbedtls=4.1.0
+v_dav1d=1.5.3
+v_libxml2=2.15.3
+v_ffmpeg=8.1
+v_mpv=5e847889b34b736760a1702b82191693d89d46d9
+v_libplacebo=27aa71a97f4daed84916936572fa6a2e1c3eedb7
+v_lcms2=lcms2.19
+v_libogg=1.3.6
 v_libvorbis=1.3.7
-v_libvpx=1.13.0
-v_libx264=023112c6f2f575c72e9f26274d183b70996fb542
-v_fftools_ffi=10070acb2c090edda86dba431f6c281145ceb221
+v_libvpx=1.15.2
+v_libx264=0480cb05fa188d37ae87e8f4fd8f1aea3711f7ee
+v_fftools_ffi=9b0d4da026d9c830702ec043c1f1f98d407025af
 v_media_kit_android_helper=b768ce102cfa9b5ddec618bb939d689d1b0899fa
 v_gas_preprocessor=ac1836309c2e77023c228b7184485597286289d3
 
 ## Pinned commit SHAs for tag-based clones
-sha_mbedtls=1873d3bfc2da771672bd8e7e8f41f57e0af77f33
-sha_dav1d=676a864a11af2c0522e1f992e770589543894686
-sha_libxml2=f507d167f1755b7eaea09fb1a44d29aab828b6d1
-sha_libvpx=d6eb9696aa72473c1a11d34d928d35a3acc0c9a9
-sha_ffmpeg=f46e514491172d15bd74b4abb1814cd2f05a763e
-sha_freetype=de8b92dd7ec634e9e2b25ef534c54a3537555c11
-sha_fribidi=6428d8469e536bcbb6e12c7b79ba6659371c435a
-sha_harfbuzz=a321c4fee56b15247c10f9aa3db7e7ccb3b8173b
-sha_libass=e8ad72accd3a84268275a9385beb701c9284e5b3
-sha_lcms2=5176347635785e53ee5cee92328f76fda766ecc6
+sha_mbedtls=0fe989b6b514192783c469039edd325fd0989806
+sha_dav1d=b546257f770768b2c88258c533da38b91a06f737
+sha_libxml2=c94eb0210183b9d7cb43f8e7fddc6be55843ef49
+sha_libvpx=d168454ecd099805c675d4a98c66f4891373302a
+sha_ffmpeg=9047fa1b084f76b1b4d065af2d743df1b40dfb56
+sha_freetype=0a0221a1347e2f1e07c395263540026e9a0aa7c7
+sha_fribidi=68162babff4f39c4e2dc164a5e825af93bda9983
+sha_harfbuzz=b0ffab42d473eb380ad0fcf42730e0f1868cbc97
+sha_libass=bbb3c7f1570a4a021e52683f3fbdf74fe492ae84
+sha_lcms2=b76633e60c8387a77268fb3359277ca25b5fd75c
 
 
 ## Dependency tree
